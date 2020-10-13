83d0945264520c6fbb8b85fe0977cf83d5dfc836
AlexJS6 <alex.spiesberger@gmail.com>
2020-10-13 12:30:23 +0200

    Added node packages and signin + signup front

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose'); // Set up your database
+
+const app = express();
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+
+module.exports = app;
