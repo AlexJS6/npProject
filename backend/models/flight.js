dea128e631e64000ca2d938003854687fd326d78
AlexJS6 <alex.spiesberger@gmail.com>
2020-10-18 12:52:06 +0200

    Added Axios package, not finished model yet

---
+++ backend/models/flight.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+const BASE_URL = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com`
+module.exports = {
+    getCompatibility: (place, currency, locale, origin, destination, partial_date) => axios({
+        method:"GET",
+        url : BASE_URL + `/zodiac_compatibility/result`,
+        headers: {
+            "content-type":"application/x-www-form-urlencoded",
+            "x-rapidapi-host":"astrology-horoscope.p.rapidapi.com",
+            "x-rapidapi-key": "yourapikey"
+        },
+        params: {
+            mystic_dob: yourBirthday,
+            mystic_dob2: theirBirthday,
+            mystic_name: yourName,
+            mystic_name2: theirName
+        }
+    })
+}
